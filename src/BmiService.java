42abee7d39a722e4075a4365f05c7cebc467efe9
Alex <yanu.z@mail.ru>
2024-01-18 17:23:34 +0300

    “second_commit”

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,8 +1,8 @@
 public class BmiService {
     public int calculate(double weight, double height) {
         double height2 = height * height;
-        double index = weight/height2;
-       return (int) index;
+        double index = weight / height2;
+        return (int) index;
 
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-       BmiService service = new BmiService();
-       int index;
-        index =service.calculate(98 ,  1.87);
+        BmiService service = new BmiService();
+        int index;
+        index = service.calculate(98, 1.87);
         System.out.println(index);
     }
 }
